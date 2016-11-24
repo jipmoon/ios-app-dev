edf8c6d72f370464699d54233857d3518848275c
jipmoon <hyungjip@Hyungs-MacBook-Pro-2.local>
2016-11-23 23:44:48 -0800

    Learning Swift material for variables

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// var and let
+var str = "Hello, playground"
+
+str = "Hi, playground"
+
+let otherStr = "Hi, Rob" //otherStr cannot be changed
+
+let name = "Jip"
+
+print("Hello " + name)
+
+// Integer (whole number)
+
+var myInt = 8
+
+print(myInt * 2)
+
+print(myInt + 100)
+
+myInt = myInt + 1
+
+myInt = myInt / 5
+
+print("myInt has value \(myInt)")
+
+let age = 20
+
+print("My name is \(name) and I am \(age) years old")
+
+// Doubles
+
+var a: Double = 8.73
+
+var b: Float = 8.73
+
+var c = 7.12
+
+print (a / c)
+
+// print (a / b) can't combine two different types
+
+ print (Double(myInt) + a)
+
+// Boolean
+
+let gameOver = false
+
+var gameOverString = String(gameOver)
+
+
+let d: Double = 5.76
+
+let e: Int = 8
+
+let result = d*Double(e)
+
+print("The product of \(d) and \(e) is \(result))")
